f912b721437df4b81d04cf803698bb2946b614f5
aymanechiheb <ch.aymane19@gmail.com>
2025-03-12 01:40:20 +0000

    feat: update API and socket URLs to use environment variables for better configuration

--- front/src/services/socket.jsx
+++ front/src/services/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:4000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const socket = io(SOCKET_URL, { transports: ['websocket'] });
 
 // Listen for the 'connect' event
